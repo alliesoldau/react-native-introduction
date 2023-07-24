e8ba34c2a4aeb9ede13d98c243e9c1ec92e61fe4
Allie Soldau <alliesoldau@gmail.com>
2023-07-24 12:22:47 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { Profil } from './components/Profil';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return (<>
+    <Text>Hey</Text>
+    <Text>Hey</Text>
+    <Text>Hey</Text>
+    <Text>Hey</Text>
+    <Text>Hey</Text>
+    <Text>Hey</Text>
+    <Text>Hey</Text>
+    <Profil/>
+  </>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Profil.jsx
@@ -0,0 +1,8 @@
+import { TouchableOpacity, Text } from "react-native";
+
+export function Profil() {
+    return <>
+    <Text>Hey hey hey</Text>
+    <TouchableOpacity><Text>Click Me!</Text></TouchableOpacity>
+    </>
+}
