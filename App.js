2596b3f50d3abdf366fda91ab7ce086a08734286
Allie Soldau <alliesoldau@gmail.com>
2023-07-24 13:59:43 -0400

    added some links and platform detection

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
-import { Text, View } from "react-native";
+import { Linking, Platform, Text } from "react-native";
 import { ProfileCard } from "./components/ProfileCard";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-import { s } from "./App.style";
 
 export default function App() {
+  function goToSocialMedia(socialMedia) {
+    let url;
+    switch (socialMedia) {
+      case "instagram":
+        url = "https://www.instagram.com/alliesoldau/";
+        break;
+      case "github":
+        url = "http://github.com/alliesoldau";
+        break;
+      case "linkedin":
+        url = "https://www.linkedin.com/in/allie-soldau/";
+        break;
+    }
+    Linking.openURL(url);
+  }
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1, justifyContent: "center", padding: 20 }}>
-        <ProfileCard />
+        <ProfileCard
+          onPressSocialMedia={goToSocialMedia}
+          firstName={"Rando"}
+          lastName={"McGee"}
+          age={28}
+        />
+        {Platform.OS === "android" && <Text>Hi on Android</Text>}
+        {Platform.OS === "ios" && <Text>Hi from iOS</Text>}
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -2,7 +2,8 @@ import { Text, View, Image, TouchableOpacity } from "react-native";
 import { s } from "./ProfileCard.style";
 import { FontAwesome } from '@expo/vector-icons';
 
-export function ProfileCard() {
+export function ProfileCard({ firstName, lastName, age, onPressSocialMedia}) {
+
     return (
     <View style= { s.container }>
         <View style={ s.header }>
@@ -10,18 +11,19 @@ export function ProfileCard() {
                 <Image style={ s.avatar } source={{ uri : "https://i.pravatar.cc/300" }}/>
             </View>
             <View style={ s.texts }>
-                <Text style={ s.name }>Rando McGee</Text>
-                <Text>Hi I am totally a real person and definitely not a computer.</Text>
+                <Text style={ s.name }>{firstName} {lastName}</Text>
+                <Text>Hi I am {age} years old and I am totally a real person and definitely not a computer.</Text>
+                <Text> { age > 100 ? "I am old af" : "I am not so old" }</Text>
             </View>
         </View>
         <View style={ s.social }>
-            <TouchableOpacity style={ s.socialBtn }>
+            <TouchableOpacity onPress={() => onPressSocialMedia("instagram")} style={ s.socialBtn }>
                 <FontAwesome name="instagram" size={24} color="hotpink" />
             </TouchableOpacity>
-            <TouchableOpacity style={ s.socialBtn }>
+            <TouchableOpacity onPress={() => onPressSocialMedia("linkedin")} style={ s.socialBtn }>
                 <FontAwesome name="linkedin-square" size={24} color="#0A66C2" />
             </TouchableOpacity>
-            <TouchableOpacity style={ s.socialBtn }>
+            <TouchableOpacity onPress={() => onPressSocialMedia("github")} style={ s.socialBtn }>
                 <FontAwesome name="github-square" size={24} color="#333" />
             </TouchableOpacity>
             
